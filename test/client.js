4b9d44fc95fbeccaa5a265fdf3db4a3b7a22daab
Maurice Prosper <maurice.prosper@ttu.edu>
2017-07-16 11:41:12 -0700

    Test direct messaging.

--- test/client.js
+++ test/client.js
@@ -34,57 +34,85 @@ describe('Connecting', () => {
 })
 
 describe('Messaging', () => {
-it('should send a message to the host', function (done) {
-	const version = 6
-	const message = {
-		hello: 'world'
-	}
-	const host = new Host({version})
+	it('should send a message to the host', function (done) {
+		const version = 6
+		const message = {
+			hello: 'world'
+		}
+		const host = new Host({version})
 
-	host.once('ready', hostID => {
-		const friend = new Client({hostID, version})
+		host.once('ready', hostID => {
+			const friend = new Client({hostID, version})
 
-		friend.once('ready', () => {
-			host.once('data', ({from, data}) => {
-				from.should.be.instanceof(MockDataConnection)
-				friend.host.connectedPeer.should.eql(from.host)
+			friend.once('ready', () => {
+				host.once('data', ({from, data}) => {
+					from.should.be.instanceof(MockDataConnection)
+					friend.host.connectedPeer.should.eql(from.host)
 
-				message.should.eql(data)
-				done()
+					message.should.eql(data)
+					done()
+				})
+
+				friend.send(message)
 			})
-
-			friend.send(message)
 		})
 	})
-})
 
-it('should send a message to a client', function (done) {
-	const version = 6
-	const message = {
-		name: 'mo',
-		age: 12.6,
-		map: new Map([[1, 5], [2, 6]])
-	}
+	it('should send a message to a client', function (done) {
+		const version = 6
+		const message = {
+			name: 'mo',
+			age: 12.6,
+			map: new Map([[1, 5], [2, 6]])
+		}
 
-	const host = new Host({version})
+		const host = new Host({version})
 
-	host.once('ready', hostID => {
-		host.once('connection', hostConnectionToFriend => {
-			friend.once('data', ({from, data}) => {
-				//(from === undefined).should.be.true()
-				from.should.equal(friend.host.id)
-				message.should.eql(data)
+		host.once('ready', hostID => {
+			host.once('connection', hostConnectionToFriend => {
+				friend.once('data', ({from, data}) => {
+					//(from === undefined).should.be.true()
+					from.should.equal(friend.host.id)
+					message.should.eql(data)
 
-				done()
+					done()
+				})
+
+				host.sendTo(hostConnectionToFriend, message)
 			})
 
-			host.sendTo(hostConnectionToFriend, message)
+			const friend = new Client({hostID, version})
 		})
-
-		const friend = new Client({hostID, version})
 	})
-})
 
+	it('should direct message another client', function (done) {
+		const version = 6
+		const message = {
+			name: 'mo',
+			age: 12.6,
+			map: new Map([[1, 5], [2, 6]])
+		}
+
+		const host = new Host({version})
+
+		host.once('ready', hostID => {
+			host.once('connection', hostConnectionToFriend => {
+				host.once('connection', hostConnectionToFriendTwo => {
+					friend2.once('data', (msg) => {
+						msg.from.should.equal(hostConnectionToFriend.id)
+						message.should.eql(msg.data)
+						done()
+					})
+
+					friend.send(message, hostConnectionToFriendTwo.id)
+				})
+
+				const friend2 = new Client({hostID, version})
+			})
+
+			const friend = new Client({hostID, version})
+		})
+	})
 })
 
 describe('Broadcasting', () => {
