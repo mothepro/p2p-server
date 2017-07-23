1941da725f15e672dad5404f6daeba562961495e
Maurice Prosper <maurice.prosper@ttu.edu>
2017-07-22 20:40:07 -0700

    Migrate main to TS.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-import Host from './src/Host'
-import Client from './src/Client'
-import * as Packer from './src/Packer'
-
-export { Host, Client, Packer }
+"use strict";
+exports.__esModule = true;
+var Host_1 = require("./src/Host");
+exports.Host = Host_1["default"];
+var Client_1 = require("./src/Client");
+exports.Client = Client_1["default"];
+var Packer = require("./src/Packer");
+exports.Packer = Packer;
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,5 @@
+import Host from './src/Host'
+import Client from './src/Client'
+import * as Packer from './src/Packer'
+
+export { Host, Client, Packer }
